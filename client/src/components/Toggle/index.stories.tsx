d55ec5371ec3727164701cd74fc57d5b1a8de5f2
jhj2713 <juhyojeong58@gmail.com>
2024-07-23 17:18:28 +0900

    chore: storybook docs 추가

--- client/src/components/Toggle/index.stories.tsx
+++ client/src/components/Toggle/index.stories.tsx
@@ -6,7 +6,20 @@ const meta = {
     title: "Toggle",
     component: Component,
     tags: ["autodocs"],
-    argTypes: {},
+    argTypes: {
+        leftText: {
+            description: "toggle의 왼쪽 칸에 들어갈 텍스트",
+        },
+        rightText: {
+            description: "toggle의 오른쪽 칸에 들어갈 텍스트",
+        },
+        isLeftSelected: {
+            description: "toggle의 왼쪽 칸 선택 여부",
+        },
+        handleToggle: {
+            description: "toggle trigger",
+        },
+    },
 } as Meta;
 
 export default meta;
