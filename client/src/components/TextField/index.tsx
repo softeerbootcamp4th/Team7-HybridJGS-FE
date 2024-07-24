e8ba1572facbc75d443f268fa37ba8e07311b395
juhyojeong <juhyojeong58@gmail.com>
2024-07-24 21:15:21 +0900

    refactor: variants 객체 파일 분리

--- client/src/components/TextField/index.tsx
+++ client/src/components/TextField/index.tsx
@@ -1,5 +1,10 @@
 import { ChangeEvent, useState } from "react";
-import { cva } from "class-variance-authority";
+import {
+    inputVariants,
+    labelVariants,
+    requiredTextVariants,
+    valueLengthTextVariants,
+} from "./index.style";
 
 export interface TextFieldProps {
     label: string;
@@ -10,45 +15,6 @@ export interface TextFieldProps {
     value: string;
     handleValueChange: (val: string) => void;
 }
-
-const labelVariants = cva(`text-n-white h-body-2-regular`, {
-    variants: {
-        isSmallSize: {
-            true: "w-[390px]",
-            false: "w-[560px]",
-        },
-    },
-});
-const inputVariants = cva(
-    `relative bg-n-neutral-950 py-[19px] px-500 rounded-400 focus-within:bg-n-neutral-500 border border-transparent focus-within:border-s-blue cursor-pointer`,
-    {
-        variants: {
-            isSmallSize: {
-                true: "w-[390px]",
-                false: "w-[560px] h-[216px]",
-            },
-        },
-    }
-);
-const requiredTextVariants = cva(`inline`, {
-    variants: {
-        isRequired: {
-            true: "text-s-blue",
-        },
-    },
-});
-const valueLengthTextVariants = cva(`absolute right-500 h-body-2-regular`, {
-    variants: {
-        isFocused: {
-            true: "text-s-blue",
-            false: "text-n-neutral-500",
-        },
-        isSmallSize: {
-            true: "translate-y-[-50%] top-[50%]",
-            false: "bottom-700",
-        },
-    },
-});
 
 export default function TextField({
     label,
