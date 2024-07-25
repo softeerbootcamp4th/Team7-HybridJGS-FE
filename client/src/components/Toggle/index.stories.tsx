3c69c5366daba4de4483fcb73a8348d044af8166
jhj2713 <juhyojeong58@gmail.com>
2024-07-25 11:02:38 +0900

    fix: toggle 디자인 수정

--- client/src/components/Toggle/index.stories.tsx
+++ client/src/components/Toggle/index.stories.tsx
@@ -25,15 +25,24 @@ const meta = {
 export default meta;
 
 const Toggle = (args: ToggleProps) => {
-    const [isLeftSelected, setIsLeftSelected] = useState(args.isLeftSelected);
+    const [selectedIdx, setSelectedIdx] = useState(args.selectedIdx);
 
-    const handleToggle = () => {
-        setIsLeftSelected((prevIsSelected) => !prevIsSelected);
+    const handleToggle = (idx: number) => {
+        setSelectedIdx(idx);
     };
 
-    return <Component {...args} isLeftSelected={isLeftSelected} handleToggle={handleToggle} />;
+    return <Component {...args} selectedIdx={selectedIdx} handleToggle={handleToggle} />;
 };
 
 export const Default = (args: ToggleProps) => (
-    <Toggle {...args} leftText="Now" rightText="Before" isLeftSelected={true} />
+    <Toggle
+        {...args}
+        options={[
+            "혜택이 늘어요",
+            "충전소가 늘어요",
+            "유지비가 줄어요",
+            "환경오염이 줄어요",
+            "소음이 줄어요",
+        ]}
+    />
 );
