bfa0438254b5aa2d8f6269ee517e442e266caf17
sooyeoniya <tndus1306403@naver.com>
2024-07-26 10:48:57 +0900

    refactor: CTAButton apply CSS 적용 스타일 cva로 변경

--- client/src/router.tsx
+++ client/src/router.tsx
@@ -1,9 +1,8 @@
 import { createBrowserRouter } from "react-router-dom";
-import CTAButton from "@/components/CTAButton";
 
 export const router = createBrowserRouter([
     {
         path: "/",
-        element: <CTAButton label="다음" onClick={() => {}} disabled={true} hasIcon={true} />,
+        element: <></>,
     },
 ]);
