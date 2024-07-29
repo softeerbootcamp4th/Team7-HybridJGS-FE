2c4a0bbdd388b8f71755748e697fd4e0c2e261c0
sooyeoniya <tndus1306403@naver.com>
2024-07-29 18:13:10 +0900

    refactor: Main 내부 섹션 feature로 분리

---
+++ client/src/features/Main/Lottery.tsx
@@ -0,0 +1,47 @@
+import CTAButton from "@/components/CTAButton";
+import ArrowIcon from "/public/assets/icons/arrow.svg?react";
+
+export default function Lottery() {
+    return (
+        <section className="flex flex-col gap-3 justify-center items-center h-screen bg-n-black">
+            <p className="h-body-1-regular text-n-white">Event 1. 추첨 이벤트</p>
+            <p className="h-heading-2-bold text-n-white">나만의 캐스퍼 일렉트릭 봇 만들기</p>
+            <p className="h-body-1-medium text-s-blue">
+                나만의 캐스퍼봇을 만들면 100% 응모 완료! 친구에게 공유하면 당첨 확률 UP!
+            </p>
+            <div className="flex gap-14	p-8 m-2 rounded-500 w-[1200px] h-[460px] bg-n-neutral-950">
+                <img
+                    src="/assets/common/casper.webp"
+                    alt="casper"
+                    className="w-[400px] h-[400px]"
+                />
+                <div className="flex flex-col gap-6">
+                    <div className="flex flex-col gap-4">
+                        <p className="h-heading-4-bold text-n-white">이벤트 기간</p>
+                        <p className="h-body-1-regular text-[#A6B2BA]">
+                            2024. 08. 23. (금) ~ 2024. 09. 05 (목)
+                        </p>
+                    </div>
+                    <div className="flex flex-col gap-4">
+                        <p className="h-heading-4-bold text-n-white">참여 방법</p>
+                        <p className="h-body-1-regular text-[#A6B2BA]">
+                            나만의 스마일 로봇 뱃지를 꾸미면 이벤트 응모가 완료돼요.
+                            <br />
+                            CASPER Electric 기대평을 작성하고 친구에게 링크 공유하면 당첨 확률이
+                            높아져요!
+                        </p>
+                    </div>
+                    <div className="flex flex-col gap-4">
+                        <p className="h-heading-4-bold text-n-white">이벤트 경품</p>
+                        <div className="flex gap-10">{/* 이벤트 경품 컴포넌트 추가 */}</div>
+                    </div>
+                </div>
+            </div>
+            <button className="flex w-[1200px] justify-end gap-1 h-body-1-regular text-n-neutral-500 hover:underline">
+                <p>다른 사람들의 스마일 로봇 뱃지 보러가기</p>
+                <ArrowIcon stroke="#637381" />
+            </button>
+            <CTAButton label="이벤트 참여하기" hasArrowIcon={true} />
+        </section>
+    );
+}
