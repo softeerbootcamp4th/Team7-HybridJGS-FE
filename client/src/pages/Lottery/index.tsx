b75cb1c129762c0429d52da0e694516d99e1092a
jhj2713 <juhyojeong58@gmail.com>
2024-07-29 15:02:18 +0900

    feat: 휠 디자인 화면 구현

--- client/src/pages/Lottery/index.tsx
+++ client/src/pages/Lottery/index.tsx
@@ -127,14 +127,49 @@ export default function Lottery() {
 
                     <div className="mt-[98px] flex gap-700">
                         <img
-                            alt="헤드램프 첫번째 이미지"
+                            alt="픽셀 디자인 첫번째 이미지"
                             src="/assets/lottery/pixel-design-1.jpg"
                             className="w-[673px] object-cover"
                         />
                         <img
-                            alt="헤드램프 두번째 이미지"
+                            alt="픽셀 디자인 두번째 이미지"
                             src="/assets/lottery/pixel-design-2.jpg"
                             className="w-[502px] object-cover"
+                        />
+                    </div>
+                </div>
+            </section>
+
+            <section className="h-screen relative flex flex-col items-center justify-center">
+                <div className="w-[1200px] flex flex-col items-end">
+                    <Description
+                        label={
+                            <>
+                                2가지
+                                <br />휠 디자인
+                            </>
+                        }
+                        title={<>알로이 휠에도 픽셀이 형상화되어 있어요</>}
+                        description={
+                            <>
+                                흔히 보던 평범한 휠의 모습이 아니예요! 가볍고 강도가 좋은 알로이
+                                휠과 타이어는
+                                <br />
+                                17인치와 15인치 모두 픽셀을 활용한 각각의 매력을 자랑해요.
+                            </>
+                        }
+                    />
+
+                    <div className="mt-[98px] flex gap-700">
+                        <img
+                            alt="휠 디자인 첫번째 이미지"
+                            src="/assets/lottery/wheel-design-1.jpg"
+                            className="w-[588px] object-cover"
+                        />
+                        <img
+                            alt="휠 디자인 두번째 이미지"
+                            src="/assets/lottery/wheel-design-2.jpg"
+                            className="w-[588px] object-cover"
                         />
                     </div>
                 </div>
