fc858619d785f5aa20e9cc000ca8c31241ae3ffa
jhj2713 <juhyojeong58@gmail.com>
2024-07-29 18:17:28 +0900

    chore: 폴더명 컨벤션 맞추기

--- client/src/pages/Lottery/index.tsx
+++ client/src/pages/Lottery/index.tsx
@@ -1,14 +1,14 @@
 import Footer from "@/components/Footer";
 import Notice from "@/components/Notice";
-import CustomDesign from "@/features/lottery/CustomDesign";
-import HeadLamp from "@/features/lottery/HeadLamp";
-import Headline from "@/features/lottery/Headline";
-import Intro from "@/features/lottery/Intro";
-import NewColor from "@/features/lottery/NewColor";
-import PixelDesign from "@/features/lottery/PixelDesign";
-import ShortCut from "@/features/lottery/ShortCut";
-import SmileBadge from "@/features/lottery/SmileBadge";
-import WheelDesign from "@/features/lottery/WheelDesign";
+import CustomDesign from "@/features/Lottery/CustomDesign";
+import HeadLamp from "@/features/Lottery/HeadLamp";
+import Headline from "@/features/Lottery/Headline";
+import Intro from "@/features/Lottery/Intro";
+import NewColor from "@/features/Lottery/NewColor";
+import PixelDesign from "@/features/Lottery/PixelDesign";
+import ShortCut from "@/features/Lottery/ShortCut";
+import SmileBadge from "@/features/Lottery/SmileBadge";
+import WheelDesign from "@/features/Lottery/WheelDesign";
 import useScrollTop from "@/hooks/useScrollTop";
 
 export default function Lottery() {
