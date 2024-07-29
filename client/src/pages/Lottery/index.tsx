cf0694c6b4f9df0435237ae841d5f156fc7364a9
jhj2713 <juhyojeong58@gmail.com>
2024-07-29 16:29:45 +0900

    feat: 캐스퍼 봇 만들러 가기 화면 구현

--- client/src/pages/Lottery/index.tsx
+++ client/src/pages/Lottery/index.tsx
@@ -1,5 +1,7 @@
 import { Link } from "react-router-dom";
 import CTAButton from "@/components/CTAButton";
+import Footer from "@/components/Footer";
+import Notice from "@/components/Notice";
 import Scroll from "@/components/Scroll";
 import Tooltip from "@/components/Tooltip";
 import Description from "@/features/lottery/Description";
@@ -284,6 +286,25 @@ export default function Lottery() {
                     absolutePosition={{ left: 884, top: 568 }}
                 />
             </Section>
+
+            <div className="h-[623px] bg-n-black flex flex-col justify-center items-center text-center">
+                <img
+                    alt="캐스퍼 아이콘"
+                    src="/assets/common/casper.webp"
+                    className="w-[258px] h-[258px]"
+                />
+                <div className="h-[26px]" />
+                <h3 className="h-heading-3-bold text-n-white">
+                    나만의 캐스퍼 일렉트릭 봇을 만들면
+                    <br />
+                    캐스퍼 일렉트릭부터 스타벅스 기프티콘까지 선물이 가득!
+                </h3>
+                <div className="h-[30px]" />
+                <CTAButton label="캐스퍼 일렉트릭 봇 만들러 가기" hasIcon onClick={() => {}} />
+            </div>
+
+            <Notice />
+            <Footer />
         </div>
     );
 }
