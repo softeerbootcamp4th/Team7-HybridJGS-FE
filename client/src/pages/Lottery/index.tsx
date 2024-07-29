c5d308e6bba72e7adc07f8402109864760b08825
jhj2713 <juhyojeong58@gmail.com>
2024-07-29 14:57:29 +0900

    feat: pixel design 화면 구현

--- client/src/pages/Lottery/index.tsx
+++ client/src/pages/Lottery/index.tsx
@@ -102,6 +102,43 @@ export default function Lottery() {
                     </div>
                 </div>
             </section>
+
+            <section className="h-screen relative flex flex-col items-center justify-center">
+                <div className="w-[1200px]">
+                    <Description
+                        label={
+                            <>
+                                Pixel
+                                <br />
+                                Design
+                            </>
+                        }
+                        title={<>픽셀 디자인은 현대 전기차만의 아이덴티티예요</>}
+                        description={
+                            <>
+                                캐스퍼 일렉트릭은 방향 지시등이 단순한 빛이 아니라, 픽셀 형태의
+                                독특한 패턴을 나타내요.
+                                <br />
+                                테일 램프와 운전대에도 이런 픽셀 디자인을 입혀 아이코닉함을 더한
+                                디테일이 있답니다.
+                            </>
+                        }
+                    />
+
+                    <div className="mt-[98px] flex gap-700">
+                        <img
+                            alt="헤드램프 첫번째 이미지"
+                            src="/assets/lottery/pixel-design-1.jpg"
+                            className="w-[673px] object-cover"
+                        />
+                        <img
+                            alt="헤드램프 두번째 이미지"
+                            src="/assets/lottery/pixel-design-2.jpg"
+                            className="w-[502px] object-cover"
+                        />
+                    </div>
+                </div>
+            </section>
         </div>
     );
 }
