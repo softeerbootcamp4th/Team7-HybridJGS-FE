07a787e9db36efa676354ed245dc01a5bcaad51e
sooyeoniya <tndus1306403@naver.com>
2024-07-29 16:07:50 +0900

    chore: CTAButton 테스트 코드 삭제

--- client/src/pages/Main/index.tsx
+++ client/src/pages/Main/index.tsx
@@ -1,22 +1,3 @@
-import CTAButton from "@/components/CTAButton";
-
 export default function Main() {
-    return (
-        <div className="flex pt-48">
-            <CTAButton
-                label="외부링크"
-                color="blue"
-                disabled={false}
-                shareIcon={true}
-                url="https://casper.hyundai.com/vehicles/electric/highlight"
-            />
-            <CTAButton
-                label="내부링크"
-                color="blue"
-                disabled={false}
-                shareIcon={true}
-                url="/lottery"
-            />
-        </div>
-    );
+    return <></>;
 }
