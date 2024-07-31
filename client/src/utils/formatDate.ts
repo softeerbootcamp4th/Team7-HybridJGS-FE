b5cae33190e9ffb000431bcaad15dd3c2359df11
sooyeoniya <tndus1306403@naver.com>
2024-07-31 18:10:11 +0900

    chore: 날짜 date 포맷 아닌 경우에 대한 예외 처리

--- client/src/utils/formatDate.ts
+++ client/src/utils/formatDate.ts
@@ -1,5 +1,11 @@
 export const formatDate = (dateString: string): string => {
     const date = new Date(dateString);
+
+    if (isNaN(date.getTime())) {
+        console.error(`Invalid date string: ${dateString}`);
+        return;
+    }
+
     const month = date.getMonth() + 1;
     const day = date.getDate();
     const DAY_OF_WEEK = ["일", "월", "화", "수", "목", "금", "토"];
