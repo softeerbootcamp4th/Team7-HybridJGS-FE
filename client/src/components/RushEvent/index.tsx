68f637bea29a8864b1ccb5e336b4b96f085e726e
sooyeoniya <tndus1306403@naver.com>
2024-07-31 18:07:16 +0900

    refactor: text class 변수로 분리

--- client/src/components/RushEvent/index.tsx
+++ client/src/components/RushEvent/index.tsx
@@ -24,14 +24,13 @@ export default function RushEvent({ date, image, prizeName }: RushEventProps) {
 
     const borderClass = isTodayEvent ? "border-s-red" : "border-n-neutral-300";
     const opacityClass = isPastEvent && "opacity-50";
+    const textClass = isTodayEvent ? "text-s-red" : "text-n-neutral-950";
 
     return (
         <div
             className={`relative w-[160px] h-[200px] py-7 px-5 rounded-500 bg-n-white flex flex-col gap-4 justify-between items-center border ${borderClass} ${opacityClass}`}
         >
-            <p
-                className={`h-body-2-bold ${isTodayEvent ? "text-s-red" : "text-n-neutral-950"} text-nowrap`}
-            >
+            <p className={`h-body-2-bold ${textClass} text-nowrap`}>
                 {isTodayEvent ? "Today" : formatDate(date)}
             </p>
             <img src={image} alt="event prize" className="object-cover" />
