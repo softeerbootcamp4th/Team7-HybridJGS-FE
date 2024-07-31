6e0c1b16b629ef6b1647c50c622062c2b64c0f52
sooyeoniya <tndus1306403@naver.com>
2024-07-31 16:51:41 +0900

    feat: 선착순 페이지 Reason 컴포넌트 구현

--- client/src/features/Rush/ReasonFirst.tsx
+++ client/src/features/Rush/ReasonFirst.tsx
@@ -1,3 +1,14 @@
+import ReasonSection from "@/features/Rush/ReasonSection.tsx";
+
 export default function ReasonFirst() {
-    return <></>;
+    return (
+        <ReasonSection subtitle="캐스퍼 일렉트릭으로 전기차를 입문해야하는 이유">
+            <p>전기차가 처음이라면</p>
+            <span>
+                <p>캐스퍼 일렉트릭의 뛰어난 </p>
+                <p className="text-s-blue">성능</p>
+                <p>에 놀라실 거예요</p>
+            </span>
+        </ReasonSection>
+    );
 }
--- client/src/features/Rush/ReasonSecond.tsx
+++ client/src/features/Rush/ReasonSecond.tsx
@@ -1,3 +1,10 @@
+import ReasonSection from "@/features/Rush/ReasonSection.tsx";
+
 export default function ReasonSecond() {
-    return <></>;
+    return (
+        <ReasonSection subtitle="캐스퍼 일렉트릭으로 전기차를 입문해야하는 이유">
+            <p className="text-s-blue">라이프스타일 그대로</p>
+            <p>캐스퍼 일렉트릭과 함께해요</p>
+        </ReasonSection>
+    );
 }
