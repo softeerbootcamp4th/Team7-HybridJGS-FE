0bad9cea007ec73042d36290394a43b856591a82
sooyeoniya <tndus1306403@naver.com>
2024-08-01 14:16:09 +0900

    design: 선착순 밸런스 게임 시간 표시 박스 UI 구현

--- client/src/features/Rush/BalanceGame.tsx
+++ client/src/features/Rush/BalanceGame.tsx
@@ -3,16 +3,28 @@ import Scroll from "@/components/Scroll";
 
 export default function BalanceGame() {
     return (
-        <section className="h-screen bg-n-white flex flex-col gap-8 justify-center items-center">
+        <section className="relative h-screen bg-n-white flex flex-col gap-8 justify-center items-center">
             <p className="h-heading-2-bold pt-10">이제 곧 하단에 밸런스 게임 주제가 공개돼요!</p>
-            <div className="flex flex-col gap-6 justify-center items-center w-[800px] h-[390px] bg-n-neutral-300 rounded-[29px]">
-                <p className="h-body-1-regular text-n-neutral-500">
-                    밸런스 게임 주제 공개까지 남은 시간
-                </p>
-                <div className="h-body-2-regular text-n-neutral-500 flex gap-[75px]">
-                    <p>Hours</p>
-                    <p>Minutes</p>
-                    <p>Seconds</p>
+            <div className="relative z-10">
+                <div className="absolute top-0 left-[-38px] w-[880px] h-[390px] rounded-[29px] overflow-hidden bg-n-white/[.16] -rotate-[4deg] z-20">
+                    <div className="w-1/2 h-full float-left bg-gradient-green blur-[40px]" />
+                    <div className="w-1/2 h-full float-right bg-gradient-red blur-[40px]" />
+                </div>
+                <div className="absolute top-0 left-[35px] w-[800px] h-[390px] rounded-[29px] overflow-hidden bg-n-white/[.16] z-5">
+                    <div className="w-1/2 h-full float-right bg-gradient-red blur-[40px]" />
+                </div>
+                <div className="absolute top-0 right-[35px] w-[800px] h-[390px] rounded-[29px] overflow-hidden bg-n-white/[.16] z-5">
+                    <div className="w-1/2 h-full float-left bg-gradient-green blur-[40px]" />
+                </div>
+                <div className="flex flex-col gap-6 justify-center items-center w-[800px] h-[390px] bg-n-white rounded-[29px] relative z-20">
+                    <p className="h-body-1-regular text-n-neutral-500">
+                        밸런스 게임 주제 공개까지 남은 시간
+                    </p>
+                    <div className="h-body-2-regular text-n-neutral-500 flex gap-[75px]">
+                        <p>Hours</p>
+                        <p>Minutes</p>
+                        <p>Seconds</p>
+                    </div>
                 </div>
             </div>
             <div className="flex flex-col justify-center items-center gap-4 my-3">
