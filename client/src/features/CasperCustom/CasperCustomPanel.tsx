85c691dab39a064f1d4481f93d45b6d4db42d4c7
jhj2713 <juhyojeong58@gmail.com>
2024-08-01 19:15:37 +0900

    refactor: 공통 코드 컴포넌트화

--- client/src/features/CasperCustom/CasperCustomPanel.tsx
+++ client/src/features/CasperCustom/CasperCustomPanel.tsx
@@ -30,6 +30,29 @@ function getCustomOptionImageItem({
     };
 }
 
+interface GetFilteredOptionsProps {
+    optionType: (typeof OPTION_TYPE)[keyof typeof OPTION_TYPE];
+    options: (typeof CASPER_OPTION)[keyof typeof CASPER_OPTION];
+    selectedOptionId?: string;
+    handleClickOption: (optionId: string) => void;
+}
+function getFilteredOptions({
+    optionType,
+    options,
+    selectedOptionId,
+    handleClickOption,
+}: GetFilteredOptionsProps) {
+    return options
+        .filter((option) => option.type === optionType)
+        .map((option) =>
+            getCustomOptionImageItem({
+                optionId: option.id,
+                isSelected: selectedOptionId === option.id || false,
+                handleClickOption: () => handleClickOption(option.id),
+            })
+        );
+}
+
 export function EyesPanel() {
     return <EyesPanelComponent />;
 }
@@ -41,24 +64,18 @@ export function MouthPanel() {
     const mouthSelectedIdx = selectedCasperIdx[CUSTOM_OPTION.MOUTH];
     const mouthSelectedOption = mouthOptions[mouthSelectedIdx];
 
-    const mouthLimited = mouthOptions
-        .filter((option) => option.type === OPTION_TYPE.LIMITED)
-        .map((option) =>
-            getCustomOptionImageItem({
-                optionId: option.id,
-                isSelected: mouthSelectedOption?.id === option.id || false,
-                handleClickOption: () => handleSelectOption(CUSTOM_OPTION.MOUTH, option.id),
-            })
-        );
-    const mouthBasic = mouthOptions
-        .filter((option) => option.type === OPTION_TYPE.BASIC)
-        .map((option) =>
-            getCustomOptionImageItem({
-                optionId: option.id,
-                isSelected: mouthSelectedOption?.id === option.id || false,
-                handleClickOption: () => handleSelectOption(CUSTOM_OPTION.MOUTH, option.id),
-            })
-        );
+    const mouthLimited = getFilteredOptions({
+        optionType: OPTION_TYPE.LIMITED,
+        options: mouthOptions,
+        selectedOptionId: mouthSelectedOption?.id,
+        handleClickOption: (optionId) => handleSelectOption(CUSTOM_OPTION.MOUTH, optionId),
+    });
+    const mouthBasic = getFilteredOptions({
+        optionType: OPTION_TYPE.BASIC,
+        options: mouthOptions,
+        selectedOptionId: mouthSelectedOption?.id,
+        handleClickOption: (optionId) => handleSelectOption(CUSTOM_OPTION.MOUTH, optionId),
+    });
 
     return (
         <SharedPanel
@@ -142,24 +159,18 @@ export function StickerPanel() {
         handleSelectOption(CUSTOM_OPTION.STICKER, stickerOptions[0].id);
     }, []);
 
-    const stickerLimited = stickerOptions
-        .filter((option) => option.type === OPTION_TYPE.LIMITED)
-        .map((option) =>
-            getCustomOptionImageItem({
-                optionId: option.id,
-                isSelected: stickerSelectedOption?.id === option.id || false,
-                handleClickOption: () => handleSelectOption(CUSTOM_OPTION.STICKER, option.id),
-            })
-        );
-    const stickerBasic = stickerOptions
-        .filter((option) => option.type === OPTION_TYPE.BASIC)
-        .map((option) =>
-            getCustomOptionImageItem({
-                optionId: option.id,
-                isSelected: stickerSelectedOption?.id === option.id || false,
-                handleClickOption: () => handleSelectOption(CUSTOM_OPTION.STICKER, option.id),
-            })
-        );
+    const stickerLimited = getFilteredOptions({
+        optionType: OPTION_TYPE.LIMITED,
+        options: stickerOptions,
+        selectedOptionId: stickerSelectedOption?.id,
+        handleClickOption: (optionId) => handleSelectOption(CUSTOM_OPTION.STICKER, optionId),
+    });
+    const stickerBasic = getFilteredOptions({
+        optionType: OPTION_TYPE.BASIC,
+        options: stickerOptions,
+        selectedOptionId: stickerSelectedOption?.id,
+        handleClickOption: (optionId) => handleSelectOption(CUSTOM_OPTION.STICKER, optionId),
+    });
 
     return (
         <SharedPanel
