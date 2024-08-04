481123e5dd113de52c0e0a51797eeccfb31da015
sooyeoniya <tndus1306403@naver.com>
2024-08-04 18:35:26 +0900

    chore: 필요 없는 테스트용 컴포넌트 호출 제거

--- client/src/pages/Rush/index.tsx
+++ client/src/pages/Rush/index.tsx
@@ -1,12 +1,9 @@
-import RushCard from "@/features/Rush/RushCard.tsx";
 import RushCardComparison from "@/features/Rush/RushCardComparison.tsx";
 import RushCardDescription from "@/features/Rush/RushCardDescription.tsx";
 
 export default function Rush() {
-    // 컴포넌트 테스트용 (storybook X)
     return (
         <div className="mt-32">
-            <RushCard />
             <RushCardComparison />
             <RushCardDescription color="red" />
         </div>
