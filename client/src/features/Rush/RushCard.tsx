1f05f0ca5ae089ebe9fbb69bf92580b87dd45a69
sooyeoniya <tndus1306403@naver.com>
2024-08-04 18:13:16 +0900

    feat: RushCard 컴포넌트 데이터 동적 업데이트 방식 적용

--- client/src/features/Rush/RushCard.tsx
+++ client/src/features/Rush/RushCard.tsx
@@ -1,16 +1,49 @@
-export default function RushCard() {
+import React from "react";
+import { cva } from "class-variance-authority";
+import {
+    CARD_COLOR,
+    CARD_DAYS,
+    CARD_DESCRIPTION,
+    CARD_SELECTED_STATUS,
+    CARD_TITLES,
+    CARD_TYPE,
+} from "@/constants/Rush/rushCard.tsx";
+
+interface RushCardProps {
+    color: (typeof CARD_COLOR)[keyof typeof CARD_COLOR];
+}
+
+const backgroundGradients = cva(
+    `flex flex-col gap-2 justify-center items-center w-[360px] h-[400px] text-n-neutral-950 rounded-800`,
+    {
+        variants: {
+            color: {
+                [CARD_COLOR.BLUE]: "bg-gradient-blue",
+                [CARD_COLOR.RED]: "bg-gradient-red",
+                [CARD_COLOR.YELLOW]: "bg-gradient-yellow",
+                [CARD_COLOR.GREEN]: "bg-gradient-green",
+            },
+        },
+        defaultVariants: {
+            color: CARD_COLOR.GREEN,
+        },
+    }
+);
+
+type CardType = (typeof CARD_TYPE)[keyof typeof CARD_TYPE];
+
+const currentDay = CARD_DAYS.DAY2;
+const cardType: CardType = CARD_TYPE.FIRST_CARD;
+const cardStatus = CARD_SELECTED_STATUS.FALSE;
+
+const cardTitle = CARD_TITLES[currentDay][cardType];
+const cardDescription = CARD_DESCRIPTION[currentDay][cardType][cardStatus];
+
+export default function RushCard({ color }: RushCardProps) {
     return (
-        <div className="flex flex-col gap-2 justify-center items-center w-[360px] h-[400px] text-n-neutral-950 bg-gradient-green rounded-800">
-            <h2 className="h-heading-2-bold text-center">
-                첫 차는
-                <br />
-                저렴해야 한다
-            </h2>
-            <p className="h-body-1-regular text-center">
-                가성비 좋게 저렴한 차로 시작해서
-                <br />
-                차근히 업그레이드하고 싶어
-            </p>
+        <div className={backgroundGradients({ color: color })}>
+            <h2 className="h-heading-2-bold text-center">{cardTitle}</h2>
+            <p className="h-body-1-regular text-center">{cardDescription}</p>
         </div>
     );
 }
--- client/src/features/Rush/RushCardComparison.tsx
+++ client/src/features/Rush/RushCardComparison.tsx
@@ -1,13 +1,14 @@
+import { CARD_COLOR } from "@/constants/Rush/rushCard.tsx";
 import RushCard from "@/features/Rush/RushCard.tsx";
 
 export default function RushCardComparison() {
     return (
         <div className="flex gap-10 justify-center items-center">
-            <RushCard />
+            <RushCard color={CARD_COLOR.RED} />
             <p className="h-heading-2-bold text-n-neutral-500 bg-n-neutral-50 rounded-800 w-[90px] h-[78px] flex justify-center items-center">
                 VS
             </p>
-            <RushCard />
+            <RushCard color={CARD_COLOR.BLUE} />
         </div>
     );
 }
