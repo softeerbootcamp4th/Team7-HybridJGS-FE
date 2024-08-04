f25b85c1968ab9f3a03926ef0d42c87ec2aae47b
sooyeoniya <tndus1306403@naver.com>
2024-08-04 17:14:29 +0900

    feat: 선착순 밸런스 게임 RushCard 컴포넌트 UI 구현

---
+++ client/src/features/Rush/RushCard.tsx
@@ -0,0 +1,16 @@
+export default function RushCard() {
+    return (
+        <div className="flex flex-col gap-2 justify-center items-center w-[360px] h-[400px] text-n-neutral-950 bg-gradient-green rounded-800">
+            <h2 className="h-heading-2-bold text-center">
+                첫 차는
+                <br />
+                저렴해야 한다
+            </h2>
+            <p className="h-body-1-regular text-center">
+                가성비 좋게 저렴한 차로 시작해서
+                <br />
+                차근히 업그레이드하고 싶어
+            </p>
+        </div>
+    );
+}
