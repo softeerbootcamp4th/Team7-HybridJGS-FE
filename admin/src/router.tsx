01b1de356c163719366cac2e4cfb4b11bf80efbb
k000927 <kimsungjin927@gmail.com>
2024-08-06 14:22:07 +0900

    chore: path 추가로 어드민 페이지와 클라이언트 페이지 배포 분리

--- admin/src/router.tsx
+++ admin/src/router.tsx
@@ -3,6 +3,6 @@ import { createBrowserRouter } from "react-router-dom";
 export const router = createBrowserRouter([
     {
         path: "/",
-        element: <p className="text-red-400 h-heading-1-bold">herd</p>,
+        element: <p className="text-red-400 h-heading-1-bold">modified</p>,
     },
 ]);
