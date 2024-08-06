8a301b7ac28e360a5b9a90411c7152d631cb80b5
sooyeoniya <tndus1306403@naver.com>
2024-08-06 11:52:06 +0900

    fix: ShortCut 페이지 motion 하나로 통일

--- client/src/features/Lottery/ShortCut.tsx
+++ client/src/features/Lottery/ShortCut.tsx
@@ -8,27 +8,27 @@ interface ShortCutProps {
 
 export default function ShortCut({ handleClickShortCutButton }: ShortCutProps) {
     return (
-        <div className="h-[623px] bg-n-black flex flex-col justify-center items-center text-center snap-start">
-            <motion.img
+        <motion.div
+            className="h-[623px] bg-n-black flex flex-col justify-center items-center text-center snap-start"
+            {...SCROLL_MOTION(ASCEND)}
+        >
+            <img
                 alt="캐스퍼 아이콘"
                 src="/assets/common/casper.webp"
                 className="w-[258px] h-[258px]"
-                {...SCROLL_MOTION(ASCEND)}
             />
             <div className="h-[26px]" />
-            <motion.h3 className="h-heading-3-bold text-n-white" {...SCROLL_MOTION(ASCEND)}>
+            <h3 className="h-heading-3-bold text-n-white">
                 나만의 캐스퍼 일렉트릭 봇을 만들면
                 <br />
                 캐스퍼 일렉트릭부터 스타벅스 기프티콘까지 선물이 가득!
-            </motion.h3>
+            </h3>
             <div className="h-[30px]" />
-            <motion.div {...SCROLL_MOTION(ASCEND)}>
-                <CTAButton
-                    label="캐스퍼 일렉트릭 봇 만들러 가기"
-                    hasArrowIcon
-                    onClick={handleClickShortCutButton}
-                />
-            </motion.div>
-        </div>
+            <CTAButton
+                label="캐스퍼 일렉트릭 봇 만들러 가기"
+                hasArrowIcon
+                onClick={handleClickShortCutButton}
+            />
+        </motion.div>
     );
 }
