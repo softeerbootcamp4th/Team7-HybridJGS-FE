daafc393eeec040e06057d60bf008a65d21b5e7c
jhj2713 <juhyojeong58@gmail.com>
2024-08-06 18:07:41 +0900

    feat: 당첨자 추첨 페이지 구현

--- admin/src/pages/Lottery/index.tsx
+++ admin/src/pages/Lottery/index.tsx
@@ -1,9 +1,35 @@
+import { ChangeEvent, useEffect, useState } from "react";
+import Button from "@/components/Button";
 import TabHeader from "@/components/TabHeader";
 
 export default function Lottery() {
+    const [giftCount, setGiftCount] = useState<number>(0);
+
+    useEffect(() => {
+        setGiftCount(363);
+    }, []);
+
+    const handleChangeInput = (e: ChangeEvent<HTMLInputElement>) => {
+        const count = parseInt(e.target.value);
+        setGiftCount(count);
+    };
+
     return (
-        <div className="flex flex-col items-center">
+        <div className="flex flex-col items-center h-screen">
             <TabHeader />
+
+            <div className="flex flex-col h-full items-center justify-center gap-8 pb-40">
+                <div className="flex border">
+                    <p className="px-6 py-4 w-[200px] bg-gray-50 h-body-1-bold">전체 참여자 수</p>
+                    <p className="px-6 py-4 w-[200px] h-body-1-regular">1000</p>
+                    <p className="px-6 py-4 w-[200px] bg-gray-50 h-body-1-bold">당첨자 수</p>
+                    <div className="self-center px-4">
+                        <input value={giftCount} onChange={handleChangeInput} />
+                    </div>
+                </div>
+
+                <Button buttonSize="lg">당첨자 추첨하기</Button>
+            </div>
         </div>
     );
 }
