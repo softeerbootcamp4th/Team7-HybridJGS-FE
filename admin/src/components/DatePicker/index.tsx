47a725b4f2c83a163245a8e81bb86058986c2f7e
jhj2713 <juhyojeong58@gmail.com>
2024-08-07 17:41:57 +0900

    refactor: svg 분리

--- admin/src/components/DatePicker/index.tsx
+++ admin/src/components/DatePicker/index.tsx
@@ -13,15 +13,7 @@ export default function DatePicker({ date, onChangeDate }: DatePickerProps) {
     return (
         <div className="relative max-w-sm">
             <div className="absolute inset-y-0 start-0 flex items-center ps-3.5 pointer-events-none">
-                <svg
-                    className="w-4 h-4 text-gray-500 dark:text-gray-400"
-                    aria-hidden="true"
-                    xmlns="http://www.w3.org/2000/svg"
-                    fill="currentColor"
-                    viewBox="0 0 20 20"
-                >
-                    <path d="M20 4a2 2 0 0 0-2-2h-2V1a1 1 0 0 0-2 0v1h-3V1a1 1 0 0 0-2 0v1H6V1a1 1 0 0 0-2 0v1H2a2 2 0 0 0-2 2v2h20V4ZM0 18a2 2 0 0 0 2 2h16a2 2 0 0 0 2-2V8H0v10Zm5-8h10a1 1 0 0 1 0 2H5a1 1 0 0 1 0-2Z" />
-                </svg>
+                <img alt="달력 아이콘" src="/assets/icons/calendar.svg" />
             </div>
             <input
                 type="date"
