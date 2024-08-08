1aa2eddff48b9627e182a49c281194db962685f9
jhj2713 <juhyojeong58@gmail.com>
2024-08-08 14:11:00 +0900

    feat: 캐스퍼 페이지 헤더 반영

--- client/src/types/sections.ts
+++ client/src/types/sections.ts
@@ -1,4 +1,6 @@
 import {
+    CASPER_CUSTOM_SECTIONS,
+    CASPER_SHOWCASE_SECTIONS,
     LOTTERY_SECTIONS,
     MAIN_SECTIONS,
     RUSH_SECTIONS,
@@ -7,8 +9,15 @@ import {
 type MainSectionKey = keyof typeof MAIN_SECTIONS;
 type LotterySectionKey = keyof typeof LOTTERY_SECTIONS;
 type RushSectionKey = keyof typeof RUSH_SECTIONS;
+type CasperCustomSectionKey = keyof typeof CASPER_CUSTOM_SECTIONS;
+type CasperShowcaseSectionKey = keyof typeof CASPER_SHOWCASE_SECTIONS;
 
-export type SectionKey = MainSectionKey | LotterySectionKey | RushSectionKey;
+export type SectionKey =
+    | MainSectionKey
+    | LotterySectionKey
+    | RushSectionKey
+    | CasperCustomSectionKey
+    | CasperShowcaseSectionKey;
 
 export interface SectionKeyProps {
     id: SectionKey;
