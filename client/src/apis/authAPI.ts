58e557be920854c633a8623fa984712c97597331
jhj2713 <juhyojeong58@gmail.com>
2024-08-08 17:27:16 +0900

    chore: fetchWithTimeout 추가

--- client/src/apis/authAPI.ts
+++ client/src/apis/authAPI.ts
@@ -1,4 +1,5 @@
 import { PostAuthRequestBody, PostAuthResponse } from "@/types/authApi";
+import { fetchWithTimeout } from "@/utils/fetchWithTimeout";
 
 const baseURL = `${import.meta.env.VITE_API_URL}/event/auth`;
 const headers = {
@@ -8,7 +9,7 @@ const headers = {
 export const AuthAPI = {
     async getAuthToken(body: PostAuthRequestBody): Promise<PostAuthResponse> {
         try {
-            const response = await fetch(`${baseURL}`, {
+            const response = await fetchWithTimeout(`${baseURL}`, {
                 method: "POST",
                 headers: headers,
                 body: JSON.stringify(body),
