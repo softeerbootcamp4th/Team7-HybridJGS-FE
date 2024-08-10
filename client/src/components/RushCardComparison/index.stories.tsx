f9d50222ccd7ed85ed63039fbee35bdaa4212b04
sooyeoniya <tndus1306403@naver.com>
2024-08-10 17:21:13 +0900

    chore: RushCard 컴포넌트 스토리북 임시 주석 처리

--- client/src/components/RushCardComparison/index.stories.tsx
+++ client/src/components/RushCardComparison/index.stories.tsx
@@ -1,91 +1,92 @@
-import { Meta, StoryFn } from "@storybook/react";
-import RushCardComparison from "@/components/RushCardComparison";
-import { CARD_DAYS, CARD_SELECTED_STATUS, CARD_TYPE } from "@/constants/Rush/rushCard";
-
-export default {
-    title: "Components/RushCardComparison",
-    component: RushCardComparison,
-    argTypes: {
-        currentDay: {
-            control: { type: "select" },
-            options: Object.values(CARD_DAYS),
-            description: "The current day for selecting the card colors and titles",
-            table: {
-                type: { summary: "CARD_DAYS" },
-                defaultValue: { summary: CARD_DAYS.DAY1 },
-            },
-        },
-        firstCardType: {
-            control: { type: "select" },
-            options: Object.values(CARD_TYPE),
-            description: "The type of the first card",
-            table: {
-                type: { summary: "CARD_TYPE" },
-                defaultValue: { summary: CARD_TYPE.FIRST_CARD },
-            },
-        },
-        secondCardType: {
-            control: { type: "select" },
-            options: Object.values(CARD_TYPE),
-            description: "The type of the second card",
-            table: {
-                type: { summary: "CARD_TYPE" },
-                defaultValue: { summary: CARD_TYPE.SECOND_CARD },
-            },
-        },
-        cardStatus: {
-            control: { type: "select" },
-            options: Object.values(CARD_SELECTED_STATUS),
-            description: "The selection status of the card",
-            table: {
-                type: { summary: "CARD_SELECTED_STATUS" },
-                defaultValue: { summary: CARD_SELECTED_STATUS.FALSE },
-            },
-        },
-    },
-} as Meta;
-
-const Template: StoryFn<typeof RushCardComparison> = (args) => <RushCardComparison {...args} />;
-
-export const FirstDay = Template.bind({});
-FirstDay.args = {
-    currentDay: CARD_DAYS.DAY1,
-    firstCardType: CARD_TYPE.FIRST_CARD,
-    secondCardType: CARD_TYPE.SECOND_CARD,
-    cardStatus: CARD_SELECTED_STATUS.FALSE,
-};
-export const SecondDay = Template.bind({});
-SecondDay.args = {
-    currentDay: CARD_DAYS.DAY2,
-    firstCardType: CARD_TYPE.FIRST_CARD,
-    secondCardType: CARD_TYPE.SECOND_CARD,
-    cardStatus: CARD_SELECTED_STATUS.FALSE,
-};
-export const ThirdDay = Template.bind({});
-ThirdDay.args = {
-    currentDay: CARD_DAYS.DAY3,
-    firstCardType: CARD_TYPE.FIRST_CARD,
-    secondCardType: CARD_TYPE.SECOND_CARD,
-    cardStatus: CARD_SELECTED_STATUS.FALSE,
-};
-export const FourthDay = Template.bind({});
-FourthDay.args = {
-    currentDay: CARD_DAYS.DAY4,
-    firstCardType: CARD_TYPE.FIRST_CARD,
-    secondCardType: CARD_TYPE.SECOND_CARD,
-    cardStatus: CARD_SELECTED_STATUS.FALSE,
-};
-export const FifthDay = Template.bind({});
-FifthDay.args = {
-    currentDay: CARD_DAYS.DAY5,
-    firstCardType: CARD_TYPE.FIRST_CARD,
-    secondCardType: CARD_TYPE.SECOND_CARD,
-    cardStatus: CARD_SELECTED_STATUS.FALSE,
-};
-export const SixthDay = Template.bind({});
-SixthDay.args = {
-    currentDay: CARD_DAYS.DAY6,
-    firstCardType: CARD_TYPE.FIRST_CARD,
-    secondCardType: CARD_TYPE.SECOND_CARD,
-    cardStatus: CARD_SELECTED_STATUS.FALSE,
-};
+// import { Meta, StoryFn } from "@storybook/react";
+// import RushCardComparison from "@/components/RushCardComparison";
+// import { CARD_DAYS, CARD_SELECTED_STATUS, CARD_TYPE } from "@/constants/Rush/rushCard";
+//
+// export default {
+//     title: "Components/RushCardComparison",
+//     component: RushCardComparison,
+//     argTypes: {
+//         currentDay: {
+//             control: { type: "select" },
+//             options: Object.values(CARD_DAYS),
+//             description: "The current day for selecting the card colors and titles",
+//             table: {
+//                 type: { summary: "CARD_DAYS" },
+//                 defaultValue: { summary: CARD_DAYS.DAY1 },
+//             },
+//         },
+//         firstCardType: {
+//             control: { type: "select" },
+//             options: Object.values(CARD_TYPE),
+//             description: "The type of the first card",
+//             table: {
+//                 type: { summary: "CARD_TYPE" },
+//                 defaultValue: { summary: CARD_TYPE.FIRST_CARD },
+//             },
+//         },
+//         secondCardType: {
+//             control: { type: "select" },
+//             options: Object.values(CARD_TYPE),
+//             description: "The type of the second card",
+//             table: {
+//                 type: { summary: "CARD_TYPE" },
+//                 defaultValue: { summary: CARD_TYPE.SECOND_CARD },
+//             },
+//         },
+//         cardStatus: {
+//             control: { type: "select" },
+//             options: Object.values(CARD_SELECTED_STATUS),
+//             description: "The selection status of the card",
+//             table: {
+//                 type: { summary: "CARD_SELECTED_STATUS" },
+//                 defaultValue: { summary: CARD_SELECTED_STATUS.FALSE },
+//             },
+//         },
+//     },
+// } as Meta;
+//
+// const Template: StoryFn<typeof RushCardComparison> = (args) => <RushCardComparison {...args} />;
+//
+// export const FirstDay = Template.bind({});
+// FirstDay.args = {
+//     currentDay: CARD_DAYS.DAY1,
+//     firstCardType: CARD_TYPE.FIRST_CARD,
+//     secondCardType: CARD_TYPE.SECOND_CARD,
+//     cardStatus: CARD_SELECTED_STATUS.FALSE,
+// };
+// export const SecondDay = Template.bind({});
+// SecondDay.args = {
+//     currentDay: CARD_DAYS.DAY2,
+//     firstCardType: CARD_TYPE.FIRST_CARD,
+//     secondCardType: CARD_TYPE.SECOND_CARD,
+//     cardStatus: CARD_SELECTED_STATUS.FALSE,
+// };
+// export const ThirdDay = Template.bind({});
+// ThirdDay.args = {
+//     currentDay: CARD_DAYS.DAY3,
+//     firstCardType: CARD_TYPE.FIRST_CARD,
+//     secondCardType: CARD_TYPE.SECOND_CARD,
+//     cardStatus: CARD_SELECTED_STATUS.FALSE,
+// };
+// export const FourthDay = Template.bind({});
+// FourthDay.args = {
+//     currentDay: CARD_DAYS.DAY4,
+//     firstCardType: CARD_TYPE.FIRST_CARD,
+//     secondCardType: CARD_TYPE.SECOND_CARD,
+//     cardStatus: CARD_SELECTED_STATUS.FALSE,
+// };
+// export const FifthDay = Template.bind({});
+// FifthDay.args = {
+//     currentDay: CARD_DAYS.DAY5,
+//     firstCardType: CARD_TYPE.FIRST_CARD,
+//     secondCardType: CARD_TYPE.SECOND_CARD,
+//     cardStatus: CARD_SELECTED_STATUS.FALSE,
+// };
+// export const SixthDay = Template.bind({});
+// SixthDay.args = {
+//     currentDay: CARD_DAYS.DAY6,
+//     firstCardType: CARD_TYPE.FIRST_CARD,
+//     secondCardType: CARD_TYPE.SECOND_CARD,
+//     cardStatus: CARD_SELECTED_STATUS.FALSE,
+// };
+export default {};
--- client/src/components/RushCardDescription/index.stories.tsx
+++ client/src/components/RushCardDescription/index.stories.tsx
@@ -1,56 +1,57 @@
-import { Meta, StoryFn } from "@storybook/react";
-import RushCardDescription from "@/components/RushCardDescription";
-import { CARD_COLOR, CARD_DAYS, CARD_SELECTED_STATUS, CARD_TYPE } from "@/constants/Rush/rushCard";
-
-export default {
-    title: "Components/RushCardDescription",
-    component: RushCardDescription,
-    argTypes: {
-        color: {
-            control: { type: "select" },
-            options: Object.values(CARD_COLOR),
-            description: "The background color of the card",
-            table: {
-                type: { summary: "CARD_COLOR" },
-                defaultValue: { summary: CARD_COLOR.GREEN },
-            },
-        },
-        day: {
-            control: { type: "select" },
-            options: Object.values(CARD_DAYS),
-            description: "The current day for selecting the card details",
-            table: {
-                type: { summary: "CARD_DAYS" },
-                defaultValue: { summary: CARD_DAYS.DAY1 },
-            },
-        },
-        cardType: {
-            control: { type: "select" },
-            options: Object.values(CARD_TYPE),
-            description: "두 개의 카드 중 하나",
-            table: {
-                type: { summary: "CARD_TYPE" },
-                defaultValue: { summary: CARD_TYPE.FIRST_CARD },
-            },
-        },
-        cardStatus: {
-            control: { type: "select" },
-            options: Object.values(CARD_SELECTED_STATUS),
-            description: "The selection status of the card",
-            table: {
-                type: { summary: "CARD_SELECTED_STATUS" },
-                defaultValue: { summary: CARD_SELECTED_STATUS.FALSE },
-            },
-        },
-    },
-} as Meta;
-
-const Template: StoryFn<typeof RushCardDescription> = (args) => <RushCardDescription {...args} />;
-
-export const Default = Template.bind({});
-Default.args = {
-    color: CARD_COLOR.GREEN,
-    day: CARD_DAYS.DAY5,
-    cardType: CARD_TYPE.FIRST_CARD,
-    cardStatus: CARD_SELECTED_STATUS.TRUE,
-};
+// import { Meta, StoryFn } from "@storybook/react";
+// import RushCardDescription from "@/components/RushCardDescription";
+// import { CARD_COLOR, CARD_DAYS, CARD_SELECTED_STATUS, CARD_TYPE } from "@/constants/Rush/rushCard";
+//
+// export default {
+//     title: "Components/RushCardDescription",
+//     component: RushCardDescription,
+//     argTypes: {
+//         color: {
+//             control: { type: "select" },
+//             options: Object.values(CARD_COLOR),
+//             description: "The background color of the card",
+//             table: {
+//                 type: { summary: "CARD_COLOR" },
+//                 defaultValue: { summary: CARD_COLOR.GREEN },
+//             },
+//         },
+//         day: {
+//             control: { type: "select" },
+//             options: Object.values(CARD_DAYS),
+//             description: "The current day for selecting the card details",
+//             table: {
+//                 type: { summary: "CARD_DAYS" },
+//                 defaultValue: { summary: CARD_DAYS.DAY1 },
+//             },
+//         },
+//         cardType: {
+//             control: { type: "select" },
+//             options: Object.values(CARD_TYPE),
+//             description: "두 개의 카드 중 하나",
+//             table: {
+//                 type: { summary: "CARD_TYPE" },
+//                 defaultValue: { summary: CARD_TYPE.FIRST_CARD },
+//             },
+//         },
+//         cardStatus: {
+//             control: { type: "select" },
+//             options: Object.values(CARD_SELECTED_STATUS),
+//             description: "The selection status of the card",
+//             table: {
+//                 type: { summary: "CARD_SELECTED_STATUS" },
+//                 defaultValue: { summary: CARD_SELECTED_STATUS.FALSE },
+//             },
+//         },
+//     },
+// } as Meta;
+//
+// const Template: StoryFn<typeof RushCardDescription> = (args) => <RushCardDescription {...args} />;
+//
+// export const Default = Template.bind({});
+// Default.args = {
+//     color: CARD_COLOR.GREEN,
+//     day: CARD_DAYS.DAY5,
+//     cardType: CARD_TYPE.FIRST_CARD,
+//     cardStatus: CARD_SELECTED_STATUS.TRUE,
+// };
+export default {};
