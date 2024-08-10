51089330e822315be213ee66db20e68af800401d
sooyeoniya <tndus1306403@naver.com>
2024-08-10 18:41:35 +0900

    refactor: Rush 결과 카드 변경된 mock 데이터 연동

--- client/src/components/RushCardResultDescription/index.tsx
+++ client/src/components/RushCardResultDescription/index.tsx
@@ -3,17 +3,15 @@ import Category from "@/components/Category";
 import {
     CARD_COLOR,
     CARD_DAYS,
-    CARD_DESCRIPTION,
-    CARD_SELECTED_STATUS,
-    CARD_TITLES,
+    CARD_OPTIONS,
+    CARD_RESULTS,
     CARD_TYPE,
-} from "@/constants/Rush/rushCard.tsx";
+} from "@/constants/Rush/rushCard";
 
 interface RushCardDescriptionProps {
     color: (typeof CARD_COLOR)[keyof typeof CARD_COLOR];
     day: (typeof CARD_DAYS)[keyof typeof CARD_DAYS];
     cardType: (typeof CARD_TYPE)[keyof typeof CARD_TYPE];
-    cardStatus: (typeof CARD_SELECTED_STATUS)[keyof typeof CARD_SELECTED_STATUS];
 }
 
 const backgroundGradients = cva(
@@ -30,20 +28,20 @@ const backgroundGradients = cva(
     }
 );
 
-export default function RushCardDescription({
+export default function RushCardResultDescription({
     color,
     day,
     cardType,
-    cardStatus,
 }: RushCardDescriptionProps) {
-    const title = CARD_TITLES[day][cardType];
-    const [descriptionTitle, descriptionText] = CARD_DESCRIPTION[day][cardType][cardStatus];
+    const mainTitle = CARD_OPTIONS[day][cardType].title;
+    const title = CARD_RESULTS[day][cardType].result_title;
+    const description = CARD_RESULTS[day][cardType].result_description;
 
     return (
         <div className={backgroundGradients({ color })}>
             <div className="flex flex-col gap-2 justify-center items-center w-[245px]">
                 <Category type="limited">당신의 선택</Category>
-                <h2 className="h-heading-2-bold text-center text-n-neutral-950">{title}</h2>
+                <h2 className="h-heading-2-bold text-center text-n-neutral-950">{mainTitle}</h2>
                 <span className="h-body-1-regular text-center text-s-red">
                     <p>지금 </p>
                     <p className="h-body-1-bold">63%</p>
@@ -52,8 +50,8 @@ export default function RushCardDescription({
             </div>
             <span className="flex flex-col justify-center items-center gap-3 w-[480px] h-[352px] rounded-800 bg-[url('/assets/main/car-2.jpg')] bg-no-repeat bg-center bg-cover text-n-white">
                 <p className="text-center">CASPER Electric</p>
-                <h2 className="h-heading-2-bold text-center">{descriptionTitle}</h2>
-                <p className="h-body-1-bold text-center">{descriptionText}</p>
+                <h2 className="h-heading-2-bold text-center">{title}</h2>
+                <p className="h-body-1-bold text-center">{description}</p>
             </span>
         </div>
     );
