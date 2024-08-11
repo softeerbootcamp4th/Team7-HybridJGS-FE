a641fa72bd1ff08b21bac5c43b7aaf304e042528
sooyeoniya <tndus1306403@naver.com>
2024-08-11 12:00:37 +0900

    feat: 밸런스 게임 카드 선택 페이지 구현

--- client/src/features/Rush/BalanceGame/CardOptions.tsx
+++ client/src/features/Rush/BalanceGame/CardOptions.tsx
@@ -1,3 +1,15 @@
+import { motion } from "framer-motion";
+import RushCardComparison from "@/components/RushCardComparison";
+import { ASCEND, SCROLL_MOTION } from "@/constants/animation.ts";
+
 export default function CardOptions() {
-    return <div>test</div>;
+    return (
+        <motion.div
+            className="flex flex-col gap-8 justify-center items-center"
+            {...SCROLL_MOTION(ASCEND)}
+        >
+            <p className="h-heading-2-bold pt-10">2개의 후보 중 하나를 선택해주세요</p>
+            <RushCardComparison />
+        </motion.div>
+    );
 }
