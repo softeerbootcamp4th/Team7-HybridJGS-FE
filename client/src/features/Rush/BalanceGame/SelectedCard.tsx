f4a8428bc8ecbadacaf073638fe7161e653f1c71
sooyeoniya <tndus1306403@naver.com>
2024-08-11 14:25:49 +0900

    chore: RushCardResultDescription Props 수정

--- client/src/features/Rush/BalanceGame/SelectedCard.tsx
+++ client/src/features/Rush/BalanceGame/SelectedCard.tsx
@@ -1,6 +1,7 @@
 import { motion } from "framer-motion";
 import RushCardResultDescription from "@/components/RushCardResultDescription";
 import RushCountDown from "@/components/RushCountDown";
+import { CARD_COLOR, CARD_DAYS, CARD_TYPE } from "@/constants/Rush/rushCard.ts";
 import { ASCEND, SCROLL_MOTION } from "@/constants/animation.ts";
 import useCountDown from "@/hooks/useCountDown.ts";
 
@@ -13,7 +14,11 @@ export default function SelectedCard() {
             {...SCROLL_MOTION(ASCEND)}
         >
             <RushCountDown countdown={countdown} />
-            <RushCardResultDescription color="red" day={1} cardType="LEFT_OPTIONS" />
+            <RushCardResultDescription
+                color={CARD_COLOR.RED}
+                day={CARD_DAYS.DAY1}
+                cardType={CARD_TYPE.LEFT_OPTIONS}
+            />
         </motion.div>
     );
 }
