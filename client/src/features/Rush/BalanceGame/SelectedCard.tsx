72bd7a6579b7ba311656ebfbfd58d0afcf5be928
sooyeoniya <tndus1306403@naver.com>
2024-08-11 17:28:23 +0900

    feat: SelectedCard 내부 컴포넌트 구조 변경 및 구현

--- client/src/features/Rush/BalanceGame/SelectedCard.tsx
+++ client/src/features/Rush/BalanceGame/SelectedCard.tsx
@@ -1,12 +1,67 @@
+import { useState } from "react";
 import { motion } from "framer-motion";
+import RushCardCurrentRatio from "@/components/RushCardCurrentRatio";
 import RushCardResultDescription from "@/components/RushCardResultDescription";
 import RushCountDown from "@/components/RushCountDown";
 import { CARD_COLOR, CARD_DAYS, CARD_TYPE } from "@/constants/Rush/rushCard.ts";
 import { ASCEND, SCROLL_MOTION } from "@/constants/animation.ts";
 import useCountDown from "@/hooks/useCountDown.ts";
+import ArrowLeftIcon from "/public/assets/icons/arrow-line-left.svg?react";
+import ArrowRightIcon from "/public/assets/icons/arrow-line-right.svg?react";
+
+interface SelectedCardProps {
+    onClick: () => void;
+}
+
+function SelectedCardDescription({ onClick }: SelectedCardProps) {
+    return (
+        <div className="relative flex gap-10">
+            <RushCardResultDescription
+                color={CARD_COLOR.RED}
+                day={CARD_DAYS.DAY4}
+                cardType={CARD_TYPE.LEFT_OPTIONS}
+            />
+            <div className="absolute flex items-center justify-center">
+                <button
+                    className="flex flex-col gap-1 justify-center items-center text-center cursor-pointer"
+                    onClick={onClick}
+                >
+                    <p className="text-n-neutral-950 text-nowrap hover:underline">
+                        전체 결과 보러가기
+                    </p>
+                    <ArrowRightIcon stroke="#22252A" />
+                </button>
+            </div>
+        </div>
+    );
+}
+
+function SelectedCardCurrentRatio({ onClick }: SelectedCardProps) {
+    return (
+        <div className="relative flex gap-10">
+            <div className="absolute flex items-center justify-center">
+                <button
+                    className="flex flex-col gap-1 justify-center items-center text-center cursor-pointer"
+                    onClick={onClick}
+                >
+                    <p className="text-n-neutral-950 text-nowrap hover:underline">
+                        이전 화면으로 돌아가기
+                    </p>
+                    <ArrowLeftIcon stroke="#22252A" />
+                </button>
+            </div>
+            <RushCardCurrentRatio />
+        </div>
+    );
+}
 
 export default function SelectedCard() {
+    const [isDescriptionVisible, setIsDescriptionVisible] = useState(true);
     const countdown = useCountDown(5);
+
+    const handleChangeCardResult = () => {
+        setIsDescriptionVisible((prev) => !prev);
+    };
 
     return (
         <motion.div
@@ -14,11 +69,11 @@ export default function SelectedCard() {
             {...SCROLL_MOTION(ASCEND)}
         >
             <RushCountDown countdown={countdown} />
-            <RushCardResultDescription
-                color={CARD_COLOR.RED}
-                day={CARD_DAYS.DAY1}
-                cardType={CARD_TYPE.LEFT_OPTIONS}
-            />
+            {isDescriptionVisible ? (
+                <SelectedCardDescription onClick={handleChangeCardResult} />
+            ) : (
+                <SelectedCardCurrentRatio onClick={handleChangeCardResult} />
+            )}
         </motion.div>
     );
 }
