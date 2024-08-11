f2ecf821380fee8e9207aa346a282eda07fef771
sooyeoniya <tndus1306403@naver.com>
2024-08-11 14:15:10 +0900

    feat: RushCard 선택 POST API 연동 및 카드 옵션 타입 number로 변경

--- client/src/components/RushCard/index.tsx
+++ client/src/components/RushCard/index.tsx
@@ -5,10 +5,11 @@ interface RushCardProps {
     color: (typeof CARD_COLOR)[keyof typeof CARD_COLOR];
     title: string;
     description: string;
+    onClick?: () => void;
 }
 
 const backgroundGradients = cva(
-    `flex flex-col gap-2 justify-center items-center w-[360px] h-[400px] text-n-neutral-950 rounded-800 p-10 break-keep`,
+    `flex flex-col gap-2 justify-center items-center w-[360px] h-[400px] text-n-neutral-950 rounded-800 p-10 break-keep cursor-pointer`,
     {
         variants: {
             color: {
@@ -24,9 +25,9 @@ const backgroundGradients = cva(
     }
 );
 
-export default function RushCard({ color, title, description }: RushCardProps) {
+export default function RushCard({ color, title, description, onClick }: RushCardProps) {
     return (
-        <div className={backgroundGradients({ color })}>
+        <div className={backgroundGradients({ color })} onClick={onClick}>
             <h2 className="h-heading-2-bold text-center max-w-58">{title}</h2>
             <p className="h-body-1-regular text-center max-w-56">{description}</p>
         </div>
--- client/src/components/RushCardComparison/index.tsx
+++ client/src/components/RushCardComparison/index.tsx
@@ -1,7 +1,11 @@
+import { useCookies } from "react-cookie";
+import { RushAPI } from "@/apis/rushAPI.ts";
 import RushCard from "@/components/RushCard/index.tsx";
+import { COOKIE_TOKEN_KEY } from "@/constants/Auth/token.ts";
 import { CARD_COLORS, CARD_DAYS, CARD_OPTIONS, CARD_TYPE } from "@/constants/Rush/rushCard";
 
 export default function RushCardComparison() {
+    const [cookies] = useCookies([COOKIE_TOKEN_KEY]);
     const currentDay: (typeof CARD_DAYS)[keyof typeof CARD_DAYS] = CARD_DAYS.DAY2;
 
     const leftOptionColor = CARD_COLORS[currentDay][CARD_TYPE.LEFT_OPTIONS];
@@ -13,12 +17,24 @@ export default function RushCardComparison() {
     const leftOptionDescription = CARD_OPTIONS[currentDay][CARD_TYPE.LEFT_OPTIONS].description;
     const rightOptionDescription = CARD_OPTIONS[currentDay][CARD_TYPE.RIGHT_OPTIONS].description;
 
+    const handleCardSelection = async (optionId: number) => {
+        const token = cookies.token;
+        if (!token) return;
+
+        try {
+            await RushAPI.postSelectedRushOptionApply(token, optionId);
+        } catch (error) {
+            console.error("Error: ", error);
+        }
+    };
+
     return (
         <div className="flex gap-10 justify-center items-center">
             <RushCard
                 color={leftOptionColor}
                 title={leftOptionTitle}
                 description={leftOptionDescription}
+                onClick={() => handleCardSelection(CARD_TYPE.LEFT_OPTIONS)}
             />
             <p className="h-heading-2-bold text-n-neutral-500 bg-n-neutral-50 rounded-800 w-[90px] h-[78px] flex justify-center items-center">
                 VS
@@ -27,6 +43,7 @@ export default function RushCardComparison() {
                 color={rightOptionColor}
                 title={rightOptionTitle}
                 description={rightOptionDescription}
+                onClick={() => handleCardSelection(CARD_TYPE.RIGHT_OPTIONS)}
             />
         </div>
     );
