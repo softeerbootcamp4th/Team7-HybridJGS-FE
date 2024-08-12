e0ce8515095ad7b3d90b710239f38822fea24484
jhj2713 <juhyojeong58@gmail.com>
2024-08-12 12:22:26 +0900

    fix: 누락된 CookiesProvider 추가

--- admin/src/components/Layout/index.tsx
+++ admin/src/components/Layout/index.tsx
@@ -1,3 +1,4 @@
+import { CookiesProvider } from "react-cookie";
 import { ErrorBoundary } from "react-error-boundary";
 import { Outlet } from "react-router-dom";
 import ErrorElement from "@/pages/ErrorElement";
@@ -6,12 +7,14 @@ import Header from "../Header";
 export default function Layout() {
     return (
         <ErrorBoundary fallback={<ErrorElement />}>
-            <div className="overflow-hidden h-screen">
-                <Header />
-                <div className="mb-[80px]">
-                    <Outlet />
+            <CookiesProvider>
+                <div className="overflow-hidden h-screen">
+                    <Header />
+                    <div className="mb-[80px]">
+                        <Outlet />
+                    </div>
                 </div>
-            </div>
+            </CookiesProvider>
         </ErrorBoundary>
     );
 }
