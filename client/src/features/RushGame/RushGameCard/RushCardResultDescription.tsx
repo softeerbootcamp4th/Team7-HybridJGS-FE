8090a12ab797bfa534ed958f47089c36b53191b6
sooyeoniya <tndus1306403@naver.com>
2024-08-13 12:08:26 +0900

    feat: 사용자가 선택한 카드에 대한 결과 카드 API 연동

--- client/src/features/RushGame/RushGameCard/RushCardResultDescription.tsx
+++ client/src/features/RushGame/RushGameCard/RushCardResultDescription.tsx
@@ -1,18 +1,11 @@
+import { useEffect, useState } from "react";
 import { cva } from "class-variance-authority";
+import { useCookies } from "react-cookie";
+import { RushAPI } from "@/apis/rushAPI.ts";
 import Category from "@/components/Category";
-import {
-    CARD_COLOR,
-    CARD_DAYS,
-    CARD_OPTIONS,
-    CARD_RESULTS,
-    CARD_TYPE,
-} from "@/constants/Rush/rushCard.ts";
-
-interface RushCardDescriptionProps {
-    color: (typeof CARD_COLOR)[keyof typeof CARD_COLOR];
-    day: (typeof CARD_DAYS)[keyof typeof CARD_DAYS];
-    cardType: (typeof CARD_TYPE)[keyof typeof CARD_TYPE];
-}
+import { COOKIE_TOKEN_KEY } from "@/constants/Auth/token.ts";
+import { CARD_COLOR } from "@/constants/Rush/rushCard.ts";
+import { GetRushOptionResultResponse } from "@/types/rushApi.ts";
 
 const backgroundGradients = cva(
     `flex gap-[35px] w-[834px] h-[400px] bg-gradient-green rounded-800 py-6 px-[37px] justify-between break-keep`,
@@ -28,14 +21,26 @@ const backgroundGradients = cva(
     }
 );
 
-export default function RushCardResultDescription({
-    color,
-    day,
-    cardType,
-}: RushCardDescriptionProps) {
-    const mainText = CARD_OPTIONS[day][cardType].mainText;
-    const resultMainText = CARD_RESULTS[day][cardType].resultMainText;
-    const resultSubText = CARD_RESULTS[day][cardType].resultSubText;
+export default function RushCardResultDescription() {
+    const [userResultData, setUserResultData] = useState<GetRushOptionResultResponse>();
+    const [cookies] = useCookies([COOKIE_TOKEN_KEY]);
+
+    useEffect(() => {
+        (async () => {
+            // TODO: 사용자가 선택한 카드 옵션(1 or 2) Context에 저장해서 받기
+            const userResultData = await RushAPI.getRushOptionResult(cookies[COOKIE_TOKEN_KEY], 1);
+            setUserResultData(userResultData);
+        })();
+    }, []);
+
+    const {
+        mainText: mainText = "",
+        resultMainText: resultMainText = "",
+        resultSubText: resultSubText = "",
+    } = userResultData || {};
+
+    // TODO: RushCard에서 선택한 색상 가져오기
+    const color = CARD_COLOR.RED;
 
     return (
         <div className={backgroundGradients({ color })}>
@@ -46,6 +51,7 @@ export default function RushCardResultDescription({
                 </h2>
                 <span className="h-body-1-regular text-center text-s-red">
                     <p>지금 </p>
+                    {/* TODO: 실시간 비율 Context로 저장해서 넣기 */}
                     <p className="h-body-1-bold">63%</p>
                     <p>가 선택했어요</p>
                 </span>
