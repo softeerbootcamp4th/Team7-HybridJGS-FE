461df268a461104b6b913c67f31442e368bdc25e
sooyeoniya <tndus1306403@naver.com>
2024-08-14 19:43:20 +0900

    feat: 프로그래스바 비율에 따른 CSS 반영

--- client/src/features/RushGame/RushGameCard/RushProgressBar.tsx
+++ client/src/features/RushGame/RushGameCard/RushProgressBar.tsx
@@ -1,3 +1,4 @@
+import { VariantProps, cva } from "class-variance-authority";
 import { useRushGameContext } from "@/hooks/useRushGameContext.ts";
 
 interface RushProgressBarProps {
@@ -5,24 +6,53 @@ interface RushProgressBarProps {
     rightOptionRatio: number;
 }
 
-// TODO: 비율대로 프로그래스바 움직이는 로직 구현
+const barStyle = cva("flex items-center", {
+    variants: {
+        color: {
+            green: "bg-gradient-green",
+            red: "bg-gradient-red",
+        },
+        status: {
+            winning: "text-n-neutral-950",
+            losing: "text-n-neutral-500",
+        },
+        textAlign: {
+            left: "justify-start",
+            right: "justify-end",
+        },
+    },
+    defaultVariants: {
+        status: "winning",
+    },
+});
+
+type BarStyleProps = VariantProps<typeof barStyle>;
+
+interface BarProps extends BarStyleProps {
+    ratio: number;
+}
+
+function Bar({ ratio, color, status, textAlign }: BarProps) {
+    return (
+        <span className={barStyle({ color, status, textAlign })} style={{ width: `${ratio}%` }}>
+            <p className="px-3">{ratio}%</p>
+        </span>
+    );
+}
+
 export default function RushProgressBar({
     leftOptionRatio,
     rightOptionRatio,
 }: RushProgressBarProps) {
     const { gameState } = useRushGameContext();
+    const isCompleted = gameState.phase === "COMPLETED";
+    const leftStatus = isCompleted && leftOptionRatio < rightOptionRatio ? "losing" : "winning";
+    const rightStatus = isCompleted && rightOptionRatio < leftOptionRatio ? "losing" : "winning";
+
     return (
-        <div className="h-heading-3-bold h-[66px] flex justify-between text-n-neutral-950">
-            <p
-                className={`bg-gradient-green flex items-center w-1/2 ${gameState.phase === "COMPLETED" && leftOptionRatio < rightOptionRatio && "text-n-neutral-500"}`}
-            >
-                {leftOptionRatio}%
-            </p>
-            <p
-                className={`bg-gradient-red flex items-center w-1/2 ${gameState.phase === "COMPLETED" && rightOptionRatio < leftOptionRatio && "text-n-neutral-500"}`}
-            >
-                {rightOptionRatio}%
-            </p>
+        <div className="h-heading-3-bold h-[66px] flex justify-between">
+            <Bar ratio={leftOptionRatio} color="green" status={leftStatus} textAlign="left" />
+            <Bar ratio={rightOptionRatio} color="red" status={rightStatus} textAlign="right" />
         </div>
     );
 }
