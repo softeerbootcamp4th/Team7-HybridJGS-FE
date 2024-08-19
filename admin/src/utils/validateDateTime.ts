31f8bf37328d01cdf351941e326df7da0cb458e0
jhj2713 <juhyojeong58@gmail.com>
2024-08-19 10:10:28 +0900

    chore: validate 문구 수정

--- admin/src/utils/validateDateTime.ts
+++ admin/src/utils/validateDateTime.ts
@@ -9,13 +9,13 @@ export function validateDateTime(
             return "시작 날짜와 시간은 현재 시간보다 빠를 수 없습니다!";
         }
         if (startDateTime > endDateTime) {
-            return "시작 날짜와 시간이 종료 날짜와 시간보다 빠를 수 없습니다!";
+            return "시작 날짜와 시간이 종료 날짜와 시간보다 느릴 수 없습니다!";
         }
     }
 
     if (key === "endDate" || key === "endTime") {
         if (endDateTime < startDateTime) {
-            return "종료 날짜와 시간이 시작 날짜와 시간보다 느릴 수 없습니다!";
+            return "종료 날짜와 시간이 시작 날짜와 시간보다 빠를 수 없습니다!";
         }
         if (endDateTime < currentDateTime) {
             return "종료 날짜와 시간은 현재 시간보다 빠를 수 없습니다!";
