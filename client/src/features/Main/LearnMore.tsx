c38d1e3ac873727debc69c5bcc7bafc68c363698
jhj2713 <juhyojeong58@gmail.com>
2024-08-20 21:09:05 +0900

    chore: 메인페이지 lazy loading 설정

--- client/src/features/Main/LearnMore.tsx
+++ client/src/features/Main/LearnMore.tsx
@@ -2,26 +2,32 @@ import { memo } from "react";
 import { motion } from "framer-motion";
 import CTAButton from "@/components/CTAButton";
 import { ASCEND, SCROLL_MOTION } from "@/constants/animation.ts";
+import useLazyLoading from "@/hooks/useLazyLoading";
 import { SectionKeyProps } from "@/types/sections.ts";
 
 function LearnMore({ id }: SectionKeyProps) {
+    const { isInView, targetRef } = useLazyLoading();
+
     return (
         <section
             id={id}
+            ref={targetRef}
             className="flex flex-col gap-6 justify-center items-center h-[76.5vh] bg-[url('/assets/main/car-2.jpg')] bg-no-repeat bg-cover snap-start"
         >
-            <motion.div
-                className="flex flex-col gap-3 justify-center items-center"
-                {...SCROLL_MOTION(ASCEND)}
-            >
-                <p className="h-heading-3-bold text-n-white">나의 첫 전기차</p>
-                <p className="h-heading-1-bold text-n-white">CASPER Electric</p>
-                <CTAButton
-                    label="더 알아보러 가기"
-                    hasShareIcon={true}
-                    url="https://casper.hyundai.com/vehicles/electric/highlight"
-                />
-            </motion.div>
+            {isInView && (
+                <motion.div
+                    className="flex flex-col gap-3 justify-center items-center"
+                    {...SCROLL_MOTION(ASCEND)}
+                >
+                    <p className="h-heading-3-bold text-n-white">나의 첫 전기차</p>
+                    <p className="h-heading-1-bold text-n-white">CASPER Electric</p>
+                    <CTAButton
+                        label="더 알아보러 가기"
+                        hasShareIcon={true}
+                        url="https://casper.hyundai.com/vehicles/electric/highlight"
+                    />
+                </motion.div>
+            )}
         </section>
     );
 }
